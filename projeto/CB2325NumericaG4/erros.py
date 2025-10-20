6773db04d91c2655383063378c8a26fb7283a69e
Guilherme Willemen <al.guilherme.castro@impatech.edu.br>
2025-10-20 16:13:33 -0300

    Commit inicial organizando o projeto em diretórios

---
+++ projeto/CB2325NumericaG4/erros.py
@@ -0,0 +1,5 @@
+def erro_relativo():
+    return "Erro relativo meu fio"
+
+def erro_absoluto():
+    return "404"
---
+++ projeto/main.py
@@ -0,0 +1,11 @@
+from CB2325NumericaG4.erros import erro_absoluto, erro_relativo
+
+def main():
+    print(erro_relativo())
+    print(erro_absoluto())
+
+
+if __name__ == "__main__":
+    main()
+
+    pass
