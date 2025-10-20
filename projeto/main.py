c46a096b46ce4450a0a12f96e63aa15cb7456784
Guilherme Willemen <al.guilherme.castro@impatech.edu.br>
2025-10-20 15:59:28 -0300

    Commit inicial do projeto cirando a organização em diretóriios

---
+++ projeto/main.py
@@ -0,0 +1,17 @@
+
+from CB2325NumericaG4.erros import erro_absoluto, erro_relativo
+
+def main():
+    ''''
+    Funcao main() do arquivo main.py
+    Pode-se adiconar testes
+    e implementações aqui, como exemplo
+    '''
+
+    print(erro_absoluto())
+    print(erro_relativo())
+
+
+if __name__ == "__main__":
+    main()
+    pass
