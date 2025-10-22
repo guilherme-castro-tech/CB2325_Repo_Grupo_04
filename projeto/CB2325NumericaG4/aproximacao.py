0f9fc3daf1b5bc75ce1e151f93e2e39ce5c6589f
NatanSpohr <al.natan.spohr@impatech.edu.br>
2025-10-22 01:49:43 -0300

    Inicia a seção de aproximação
    Um primeiro commit simples para iniciar a produção do código

---
+++ projeto/CB2325NumericaG4/aproximacao.py
@@ -0,0 +1,13 @@
+# Aproximação Funcional
+# Alunos Responsáveis: Natan Spohr, Pedro Paulo
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Matriz:
+  pass # __init__, __str__, __add__, __mul__, transposta, inversa ...
+  '''O professor ainda decidirá se essa classe deve ser implementada ou se poderá ser
+  usada as operações de algebra linear disponíveis no Numpy'''
+  
+def aproximação_polinomial(pontos: list, grau: int):
+  pass #Retornar uma lista com os coeficientes do polinômio calculado?
