ff5d991467288a790425318d8bbf48bfc476b4e1
Lucas Caracas <al.lucas.carlos@impatech.edu.br>
2025-10-22 22:29:53 -0300

    Método de aproximação por retângulos
    Tentei fazer um jeito mais eficiente calcular a integral. Em vez de usar um 'for' para somar os resultados um de cada vez, esse método cria uma lista com os valores de f(x), aplica o método map na lista inteira e depois soma a lista inteira. Esse método usa muita memória e quando eu tentei contornar isso o algoritmo perdia sua eficiência. 'integral_rect2' usa um método parecido com a função do trapézio

--- projeto/CB2325NumericaG4/integracao.py
+++ projeto/CB2325NumericaG4/integracao.py
@@ -14,8 +14,41 @@ def integral(funcao, a, b, n):
         s += (funcao(a + c*dx) + funcao(a + (c+1)*dx))*(dx/2) #Calcula as aproximações
     return s
 
+import numpy as np
+def integral_rect(funcao, a, b, n):
+    """
+    O primeiro parâmetro é uma função, o segundo e o 
+    terceiro são os limites de integração, o quarto é
+    em quantos retângulos a função é dividida.
+    """
+    s = 0
+    dx = (b-a)/ n
+    chunk = n // 10000
+    #vals = a + dx*np.arange(n)
+    #lista = map(funcao, vals)
+    #s = sum(lista)*dx
+    #print(chunk)
+    for start in range(0,n,chunk):
+        end = min(n,chunk+start)
+        vals = a + (dx*np.arange(start,end))
+        arr = np.fromiter(map(funcao,vals),dtype=float)
+        s += float(np.sum(arr)*dx)
+    return s
+
+def integral_rect2(funcao, a, b, n):
+    """
+    O primeiro parâmetro é uma função, o segundo e o 
+    terceiro são os limites de integração, o quarto é
+    em quantos retângulos a função é dividida.
+    """
+    dx = (b-a)/n    
+    s = 0
+    #lista = [i for i in range(n)]
+
+    for c in range(n):
+        s += (funcao(a + c*dx))*dx
+    return s
 
 if __name__ == "__main__":
-    print(integral(math.sin, 0, math.pi, 100))
-
-
+    n = 100000000
+    print(integral_rect(math.sin, 0, math.pi, n))
