7a2c3c3f00bbcbfcff959a0028d811fc15b281bd
NatanSpohr <al.natan.spohr@impatech.edu.br>
2025-10-26 18:01:08 -0300

    Acrescenta argumentos e erros
    Permite chamar a função usando listas separadas para as coordenadas. Testa algumas mensagens de erros possíveis.

--- projeto/CB2325NumericaG4/aproximacao.py
+++ projeto/CB2325NumericaG4/aproximacao.py
@@ -4,21 +4,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
   
-def poly_regression(pontos: list, grau: int):
+def poly_regression(grau: int = 1, pontos: list = None, *, x: list = None, y: list = None):
+  if pontos:
+    xcords, ycords = zip(*pontos)
+  elif x and y:
+    xcords, ycords = x, y
+  else:
+    raise TypeError("Falta de argumentos obrigatórios -> 'pontos' ou ambos 'x' e 'y'")
   
-  '''Por enquanto o código apenas aceita uma lista de tuplas,
-  e não uma lista para cada coordenada.'''
-  x, y = zip(*pontos)
+  if type(grau) != int or grau < 0:
+    raise TypeError("Argumento 'grau' deve ser inteiro não negativo")
 
   # Passo a passo para regressão polinomial.
   # Leia-se @ como produto, .T como transposta.
-  V = np.vander(x, grau+1, increasing=True) #Matriz de Vandermonde
+  V = np.vander(xcords, grau+1, increasing=True) # Matriz de Vandermonde
   V1 = V.T @ V
   V2 = np.linalg.inv(V1)
-  Y = V.T @ y
+  Y = V.T @ ycords
   coeficientes = V2 @ Y
   return coeficientes
 
-# Teste de aproximação quadrática com 4 pontos
+# Testes
 pontos = [(1, 3), (2, 4), (3, 7), (4, 5)]
-print(poly_regression(pontos, 2))
+print(poly_regression(2, pontos))
+a = [1, 2, 3, 4]
+b = [1, 4, 9, 16]
+print(poly_regression(1, x=a, y=b))
