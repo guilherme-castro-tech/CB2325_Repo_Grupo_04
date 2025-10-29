3f5c47ee565c7d8f5bac61d2a07a5367b53b1c72
PedroPauloPaiva <al.pedro.gomes@impatech.edu.br>
2025-10-29 16:31:54 -0300

    Descrição do código
    Adição de docstrig, notas e outra condição de checagem (tamanho das listas).

--- projeto/CB2325NumericaG4/aproximacao.py
+++ projeto/CB2325NumericaG4/aproximacao.py
@@ -3,30 +3,52 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-  
-def poly_regression(grau: int = 1, pontos: list = None, *, x: list = None, y: list = None):
-  if pontos:
-    xcords, ycords = zip(*pontos)
-  elif x and y:
-    xcords, ycords = x, y
-  else:
-    raise TypeError("Falta de argumentos obrigatórios -> 'pontos' ou ambos 'x' e 'y'")
-  
-  if type(grau) != int or grau < 0:
-    raise TypeError("Argumento 'grau' deve ser inteiro não negativo")
 
-  # Passo a passo para regressão polinomial.
-  # Leia-se @ como produto, .T como transposta.
-  V = np.vander(xcords, grau+1, increasing=True) # Matriz de Vandermonde
-  V1 = V.T @ V
-  V2 = np.linalg.inv(V1)
-  Y = V.T @ ycords
-  coeficientes = V2 @ Y
-  return coeficientes
+def regressao_polinomial(grau: int = 1, pontos: list = None, *, x: list = None, y: list = None):
 
-# Testes
-pontos = [(1, 3), (2, 4), (3, 7), (4, 5)]
-print(poly_regression(2, pontos))
-a = [1, 2, 3, 4]
-b = [1, 4, 9, 16]
-print(poly_regression(1, x=a, y=b))
+    """
+    Encontra a função polinomial que melhor aproxima um conjunto de pontos.
+
+    Parâmetros:     
+        grau (inteiro positivo): Indica o grau do polinômio que vai aproximar os pontos;    
+        pontos (lista): Conjunto de pontos formados por duas coordenadas (x,y);     
+        x/y: (listas): Opção alternativa. Duas listas com as coordenadas x e y para cada ponto respectivamente.
+
+    Retorna: Uma lista com os coeficientes do polinômio em ordem crescente de grau.
+    """
+
+    if pontos:                          # Analisa se o método de imput escolhido foi a lista de pontos (x,y)
+        xcoords, ycoords = zip(*pontos)
+    elif x and y:                       # Analisa se o método de imput escolhido foi as listas de coordenadas em x e y
+        xcoords, ycoords = x, y
+    else:
+        raise TypeError("Falta de argumentos obrigatórios -> 'pontos' ou ambos 'x' e 'y'")
+
+    if type(grau) != int or grau < 0:
+        raise TypeError("Argumento 'grau' deve ser inteiro não negativo")
+    
+    if len(xcoords) != len(ycoords):
+        raise ValueError("As listas de coordenadas devem ter o mesmo tamanho")
+
+    # Passo a passo para regressão polinomial (Mínimos quadrados).
+    # coeficientes = (V^T * V)^{-1} * V^T * ycords
+    # Leia-se @ como produto e .T como transposta.
+
+    V = np.vander(xcoords, grau+1, increasing=True) # Cria uma matriz de Vandermonde
+    V1 = V.T @ V                                    
+    V2 = np.linalg.inv(V1)  # Inverte a matriz V1
+    Y = V.T @ ycoords                                
+    coeficientes = V2 @ Y   # Encontra os coeficientes
+    return coeficientes
+
+if __name__ == '__main__':
+    
+    #Testes:
+    print('Teste 1:')
+    pontos = [[1, 3], [2, 4], [3, 7], [4, 5]]
+    print(regressao_polinomial(2, pontos))
+
+    print('Teste 2:')
+    a = [1, 2, 3, 4]
+    b = [1, 4, 9, 16]
+    print(regressao_polinomial(2, x=a, y=b))
