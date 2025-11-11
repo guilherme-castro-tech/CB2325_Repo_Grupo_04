9608ae3ffb8243eda0b9c7dd14f1ef3d8c861b17
Lucas Caracas <al.lucas.carlos@impatech.org.br>
2025-11-11 16:24:31 -0300

    Comentários padronizados

--- projeto/CB2325NumericaG4/integracao.py
+++ projeto/CB2325NumericaG4/integracao.py
@@ -1,51 +1,81 @@
 import math
-import pytest
 import math, random
 
-def integral_trap(funcao, a, b, n=10):        #integração por trapézios
-    """O primeiro parametro é uma função, O segundo e o 
-    Terceiro são os limites de integração, O
-     Quarto é em quantos trapezios você que aproximar a sua função"""
+def integral_trap(funcao, a, b, n=1000):
 
-    dx = (b-a)/n#Comprimento do intervalo
+    """
+    Objetivos: - "Essa função calcula a integral numérica pelo método dos trapézios".   
+
+    Parâmetros:     
+    funcao: Função a ser integrada;  
+    a (float): Limite inferior de integração;
+    b (float): Limite superior de integração;  
+    n (int): Número de subdivisões.
+
+    Retorna: "(float) Valor aproximado  da integral definida da função entre os limites a e b"
+    """
+
+    dx = (b-a)/n
     s = 0
     for c in range(n):
-        s += (funcao(a + c*dx) + funcao(a + (c+1)*dx))*(dx/2) #Calcula as aproximações
+        s += (funcao(a + c*dx) + funcao(a + (c+1)*dx))*(dx/2)
     return s
 
 
-def integral_rect(funcao, a, b, n=10):            #essa função usa uma sequência parecida com a do método de trapézios
+def integral_rect(funcao, a, b, n=1000):
+
     """
-    O primeiro parâmetro é uma função, o segundo e o 
-    terceiro são os limites de integração, o quarto é
-    em quantos retângulos a função é dividida.
+    Objetivos: - "Essa função calcula a integral numérica por retângulos".   
+
+    Parâmetros:     
+    funcao: Função a ser integrada;  
+    a (float): Limite inferior de integração;
+    b (float): Limite superior de integração;  
+    n (int): Número de subdivisões.
+
+    Retorna: "(float) Valor aproximado  da integral definida da função entre os limites a e b"
     """
+
     dx = (b-a)/n    
     s = 0
-    #lista = [i for i in range(n)]
-
     for c in range(n):
         s += (funcao(a + c*dx))*dx
     return s
 
-def integral_simpson(funcao, a, b, n=10):
-    """O primeiro parametro é uma função, O segundo e o 
-    Terceiro são os limites de integração, O
-    Quarto é em quantos trapezios você quer aproximar a sua função"""
+def integral_simpson(funcao, a, b, n=1000):
 
-    dx = (b-a)/n    #Comprimento do intervalo
+    """
+    Objetivos: - "Essa função calcula a integral numérica pelo método de Simpson".   
+
+    Parâmetros:     
+    funcao: Função a ser integrada;  
+    a (float): Limite inferior de integração;
+    b (float): Limite superior de integração;  
+    n (int): Número de subdivisões.
+
+    Retorna: "(float) Valor aproximado  da integral definida da função entre os limites a e b"
+    """
+
+    dx = (b-a)/n    
     s = 0
     for c in range(n):
         s += (funcao(a + c*dx) + 4 * funcao((a + a + c * dx + (c+1) * dx)/2) + funcao(a + (c+1)*dx))*(dx/6)         #Calcula as aproximações
     return s
     
-def monteCarlo(a, b, c, d, funcao, n=10):
-    """monte Carlo é magia"""
+def monteCarlo(a, b, c, d, funcao, n=1000):
+
     """
-    Para calcular Integrais de funções de 2 variaveis, 
-    ou seja, Calcular volume, podemos escolher n pontos no R² de forma aleatória, 
-    calcular o valor da função nesses pontos, somar todos. 
-    Após isso, fazer uma média de todos os valores e multiplicar pela área do domínio"""
+    Objetivos: - "Essa função calcula a integral numérica pelo método de Monte Carlo".   
+
+    Parâmetros:     
+    funcao: Função a ser integrada;  
+    a (float): Limite inferior de integração;
+    b (float): Limite superior de integração;  
+    n (int): Número de subdivisões.
+
+    Retorna: "(float) Valor aproximado  da integral definida da função entre os limites a e b"
+    """
+
     s = 0
     for i in range(n):
         x = random.uniform(a, b)        #Escolhe a coordenada 
@@ -54,6 +84,22 @@ def monteCarlo(a, b, c, d, funcao, n=10):
     
     media = s/n
     return media*(b-a)*(d-c)
+
+
+def func_exemplo(a, b, c):  # Nome da função em letras minusculas utilizando snake_case
+    
+    """
+    (Objetivo da função) - "Essa função faz isso utilizando esse método".   
+
+    Parâmetros:     
+    a (Tipo do parâmetro): Descrição do parâmetro;  
+    b (Tipo do parâmetro): Descrição do parâmetro;  
+    c (Tipo do parâmetro): Descrição do parâmetro.
+
+    Retorna: "Aquilo que a função retorna"
+    """
+
+    return None
 
 def test_integral_trap():
     assert abs(integral_trap(math.sin, 0, math.pi, 100000) - 2) < 1e-5
@@ -72,22 +118,3 @@ def test_monteCarlo():
     assert abs(monteCarlo(0, 1, 0, 1, f, 10000) - 0.25) < 1e-2
     g = lambda x, y: math.sin(x)*math.cos(y)
     assert abs(monteCarlo(0, math.pi/2, 0, math.pi/2, g, 10000) - 1) < 1e-2
-
-if __name__ == "__main__":
-    n = 1000             
-    '''é possível colocar um número muito alto aqui para comparar a velocidade das funções,
-    no entanto, ter um número enorme de divisões não é prático, pois ainda que o usuário necessite de uma precisão enorme,
-    o número obtido pela integral de simpson apenas com n= 1000 tem um erro de 6,8*10e-15,
-    sendo que um número floating point do python não consegue representar um número muito menor que esse, então,
-    nesse ponto, a precisão não pode ficar maior.'''
-    
-    print(integral_trap(math.sin, 0, math.pi, n*3))         #n*3
-    print(integral_rect(math.sin, 0, math.pi, n)*6)         #n*6
-    print(integral_simpson(math.sin, 0, math.pi, n*2))#n*2, as quantidades aqui são multiplicadas por 3, 6 e 2 para cancelar a diferença de velocidade entre elas, assim é possível comparar a precisão obtida por cada método depois de um mesmo período de tempo
-    #print(integral_rect(math.sin, 0, math.pi, n))
-    f = lambda x, y: math.sin(x)*math.cos(y)
-    print(monteCarlo(0, math.pi/2, 0, math.pi/2, f, n))
-    
-    pytest.main(["-v", __file__])
-
-
