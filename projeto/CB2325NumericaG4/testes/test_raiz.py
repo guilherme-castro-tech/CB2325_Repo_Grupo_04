28dc16d92ed99786b8d3fa9cd7fc6bbc960f19f2
LuisaCosta268 <al.luisa.costa@impatech.edu.br>
2025-11-13 17:11:17 -0300

    Criei os testes automatizados para o Método da Secante.

--- projeto/CB2325NumericaG4/testes/test_raiz.py
+++ projeto/CB2325NumericaG4/testes/test_raiz.py
@@ -11,6 +11,8 @@ def test_exemplo1(): # Há uma raiz não exata (decimal infinito)
     assert math.isclose(resultado_bissecao, 0.567, abs_tol=1e-3)
     resultado_newton = raiz(f1, 0, 1, tol, method='newton_raphson')
     assert math.isclose(resultado_newton, 0.567, abs_tol=1e-3)
+    resultado_secante = raiz(f1, 0, 1, tol, method='secante')
+    assert math.isclose(resultado_secante, 0.567, abs_tol=1e-3)
 
 def test_exemplo2(): # Há uma raiz exata
     f2 = lambda x: x**2 - 4
@@ -19,6 +21,8 @@ def test_exemplo2(): # Há uma raiz exata
     assert math.isclose(resultado_bissecao, 2.0, abs_tol=1e-3)
     resultado_newton = raiz(f2, 1, 3, tol, method='newton_raphson') 
     assert math.isclose(resultado_newton, 2.0, abs_tol=1e-3)
+    resultado_secante = raiz(f2, 1, 3, tol, method='secante') 
+    assert math.isclose(resultado_secante, 2.0, abs_tol=1e-3)
 
 def test_exemplo3(): # Existe uma raiz, mas não há mudança de sinal da imagem
     f3 = lambda x: abs(x)
@@ -27,6 +31,8 @@ def test_exemplo3(): # Existe uma raiz, mas não há mudança de sinal da imagem
     assert resultado_bissecao is None
     resultado_newton = raiz(f3, -1, 1, tol, method='newton_raphson')
     assert math.isclose(resultado_newton, 0.0, abs_tol=1e-3)
+    resultado_secante = raiz(f3, -1, 1, tol, method='secante')
+    assert resultado_secante is None
     
 def test_exemplo4(): # Não há raízes
     f4 = lambda x: x**2 + 4
@@ -35,3 +41,6 @@ def test_exemplo4(): # Não há raízes
     assert resultado_bissecao is None
     resultado_newton = raiz(f4, -2, 2, tol, method='newton_raphson')
     assert resultado_newton is None
+    resultado_newton = raiz(f4, -2, 2, tol, method='secante')
+    assert resultado_secante is None
+    
