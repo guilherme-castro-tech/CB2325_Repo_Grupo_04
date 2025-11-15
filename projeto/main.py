82591d2fe904bd4ffe8078bd58a523416260b435
Guilherme Willemen Pimentel de Castro <al.guilherme.castro@impatech.edu.br>
2025-11-14 21:51:18 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -1,11 +1,9 @@
 from CB2325NumericaG4.erros import erro_absoluto, erro_relativo
 
 def main():
-    print(erro_relativo())
-    print(erro_absoluto())
+    print(erro_relativo(2,2.2))
+    print(erro_absoluto(2,2.2))
 
 
 if __name__ == "__main__":
     main()
-
-    pass
